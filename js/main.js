8275052808313bc65504a591c2426f8128b0e7e6
MarkOstapchuk <79662433+MarkOstapchuk@users.noreply.github.com>
2023-12-17 23:18:02 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(function(){
   $('a[href^="#"]').on('click', function(event) {
     // отменяем стандартное действие
     event.preventDefault();
-    
     var sc = $(this).attr("href"),
         dn = $(sc).offset().top;
     /*
